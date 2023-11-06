2796492b66ff9b46fdefb4229f4965bc3e061c91
Mr-Unforgettable <rulzabhinav69@gmail.com>
2023-11-06 18:29:15 +0530

    Added user text boilerplate and chat structure.

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,40 @@
 import React from "react";
 
 const Chats = () => {
-  return <div>Chats</div>;
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/3574683/pexels-photo-3574683.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/3574683/pexels-photo-3574683.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img
+          src="https://images.pexels.com/photos/3574683/pexels-photo-3574683.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chats;
