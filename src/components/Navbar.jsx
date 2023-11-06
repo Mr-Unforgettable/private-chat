8e6083f66c80177ace9b793f73fcfa330d0e8b5a
Mr-Unforgettable <rulzabhinav69@gmail.com>
2023-11-06 18:31:45 +0530

    Added the Navbar, Search, Sidebar in the Home comp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <span className="logo">Hello Chat</span>
+      <div className="user">
+        <img
+          src="https://images.pexels.com/photos/3574683/pexels-photo-3574683.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt=""
+        />
+        <span>John</span>
+        <button>Logout</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
